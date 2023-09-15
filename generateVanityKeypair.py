5a2e820f29d7b46cf6892f19dc3b6f41937df847
John Wooten <hello@blocktransfer.dev>
2023-09-15 13:41:56 -0400

    Add partial matches for related complex keypair queries

--- generateVanityKeypair.py
+++ generateVanityKeypair.py
@@ -1,30 +1,48 @@
 from globals import *
 
 def main():
-  prefix = getINPUT("Enter the desired prefix: ")
-  firstChar = prefix[0] if prefix else ""
-  if firstChar not in ["", "A", "B", "C", "D"]:
-    sys.exit("Try a prefix starting with A/B/C/D.")
-  suffix = getINPUT("Enter the desired suffix: ")
-  inputValidation = prefix + suffix
-  if any(chars not in BASE_32_ALPHABET for chars in inputValidation):
-    sys.exit("Try base32 inputs.")
-  getSearchTime(len(inputValidation))
-  n = 0
-  startTime = time.time()
-  while True:
-    if not n % 3200:
-      showSearching(startTime)
-    n += 1
-    keypair = Keypair.random()
-    PK = keypair.public_key
-    if PK.startswith(f"G{prefix}") and PK.endswith(suffix):
-      result = f"""\n
-      \tKeypair found in {n} attempts:
-      \tPublic Key: {PK}
-      \tSecret Key: {keypair.secret}\n
-      """
-      sys.exit(result)
+  try:
+    prefix = getINPUT("Enter the desired prefix: ")
+    firstChar = prefix[0] if prefix else ""
+    if firstChar not in ["", "A", "B", "C", "D"]:
+      sys.exit("Try a prefix starting with A/B/C/D.")
+    suffix = getINPUT("Enter the desired suffix: ")
+    if any(chars not in BASE_32_ALPHABET for chars in prefix + suffix):
+      sys.exit("Try base32 inputs.")
+    prefixLen = len(prefix)
+    suffixLen = len(suffix)
+    getSearchTime(prefixLen + suffixLen)
+    searchingMoreForPrefix = prefixLen > suffixLen
+    prefix = f"G{prefix}"
+    n = 0
+    startTime = time.time()
+    while True:
+      if not n % 3200:
+        showSearching(startTime)
+      n += 1
+      keypair = Keypair.random()
+      PK = keypair.public_key
+      prefixMatch = PK.startswith(prefix)
+      suffixMatch = PK.endswith(suffix)
+      if prefixMatch and suffixMatch:
+        result = f"""\n
+        \tKeypair found in {n} attempts:
+        \tPublic Key: {PK}
+        \tSecret Key: {keypair.secret}\n
+        """
+        sys.exit(result)
+      if(
+        (prefixMatch and searchingMoreForPrefix) or
+        (suffixMatch and not searchingMoreForPrefix)
+      ):
+        partialMatch = [
+          "\r  Found partial match:             ",
+          f"\n  - Public Key: {PK}",
+          f"\n  - Secret Key: {keypair.secret}\n"
+        ]
+        sys.stdout.write("".join(partialMatch))
+  except KeyboardInterrupt:
+    sys.exit("\nUser ended keypair search.")
 
 def getSearchTime(searchSpan):
   match searchSpan:

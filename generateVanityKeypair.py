0c9647ef560312ee23f78cb0c11eb7fb470884f7
John Wooten <hello@blocktransfer.dev>
2023-09-15 14:22:22 -0400

    Clarify diction, add option to disable partial matches

--- generateVanityKeypair.py
+++ generateVanityKeypair.py
@@ -2,20 +2,27 @@ from globals import *
 
 def main():
   try:
+    # Inputs #
     prefix = getINPUT("Enter the desired prefix: ")
     firstChar = prefix[0] if prefix else ""
     if firstChar not in ["", "A", "B", "C", "D"]:
       sys.exit("Try a prefix starting with A/B/C/D.")
     suffix = getINPUT("Enter the desired suffix: ")
+    
+    # Input Validation #
     if any(chars not in BASE_32_ALPHABET for chars in prefix + suffix):
       sys.exit("Try base32 inputs.")
     prefixLen = len(prefix)
     suffixLen = len(suffix)
-    getSearchTime(prefixLen + suffixLen)
+    approxSearchTime = validateSearchSpan(prefixLen + suffixLen)
     searchingMoreForPrefix = prefixLen > suffixLen
     prefix = f"G{prefix}"
-    n = 0
+    
+    # Main Keygen #
+    partials = getINPUT("Would you like partial matches? (Y/n): ") == "Y"
+    print(approxSearchTime)
     startTime = time.time()
+    n = 0
     while True:
       if not n % 3200:
         showSearching(startTime)
@@ -31,34 +38,35 @@ def main():
         \tSecret Key: {keypair.secret}\n
         """
         sys.exit(result)
-      if(
-        (prefixMatch and searchingMoreForPrefix) or
-        (suffixMatch and not searchingMoreForPrefix)
-      ):
-        partialMatch = [
-          "\r  Found partial match:             ",
-          f"\n  - Public Key: {PK}",
-          f"\n  - Secret Key: {keypair.secret}\n"
-        ]
-        sys.stdout.write("".join(partialMatch))
+      if not partials: continue
+      if searchingMoreForPrefix:
+        if not prefixMatch: continue
+      else:
+        if not suffixMatch: continue
+      partialMatch = [
+        "\r Partial match:                  ",
+        f"\n  Public Key: {PK}",
+        f"\n  Secret Key: {keypair.secret}\n"
+      ]
+      sys.stdout.write("".join(partialMatch))
   except KeyboardInterrupt:
     sys.exit("\nUser ended keypair search.")
 
-def getSearchTime(searchSpan):
-  match searchSpan:
+def validateSearchSpan(totalInputLen):
+  match totalInputLen:
     case span if span > 10:
       sys.exit("Try shorter inputs.")
     case 5:
-      print("Be advised: >30 min to compute.")
+      return "Be advised: >30 min to compute."
     case 6:
-      print("Be advised: >2 hrs to compute.")
+      return "Be advised: >2 hrs to compute."
     case 7:
-      print("Be advised: >5 hrs to compute.")
+      return "Be advised: >5 hrs to compute."
     case 8:
-      print("Be advised: >16 hrs to compute.")
+      return "Be advised: >16 hrs to compute."
     case 9:
-      print("Be advised: >2 days to compute.")
+      return "Be advised: >2 days to compute."
     case 10:
-      print("Be advised: >1 week to compute.")
+      return "Be advised: >1 week to compute."
 
 print(main())
